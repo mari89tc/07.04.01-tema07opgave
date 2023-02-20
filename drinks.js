93387f6e6f2bbbf468d0e59913a392bac6e143c8
js0087 <jusu0087@stud.kea.dk>
2023-02-20 10:19:33 +0100

    cleaning kode

--- drinks.js
+++ drinks.js
@@ -38,39 +38,3 @@ function ingrediens(ing) {
 
   document.querySelector("ul").appendChild(copy);
 }
-
-//   .then((response) => response.json())
-//   .then((data) => showProducts(data));
-
-// function showProducts(tipsy) {
-//   console.log("tipsy " + tipsy);
-//   document.querySelector(".drinkname").textContent = tipsy.name;
-//   document.querySelector(".liquor").textContent = tipsy.liquor;
-//   document.querySelector(".taste").textContent = tipsy.taste;
-//   document.querySelector(".strenght").textContent = "Styrke : " + tipsy.strenght + "/5";
-//   document.querySelector(".difficulty").textContent = "Sværighedsgrad : " + tipsy.difficulty + "/5";
-//   document.querySelector(".recipe").textContent = tipsy.recipe;
-//   document.querySelector(".image-bigger").src = tipsy.image;
-
-//   // if (Array.isArray(tipsy.ingredienser)) {
-//   //   ingredienser(tipsy.ingredienser);
-//   // } else {
-//   //   console.error("Ingredients is not an array.");
-//   // }
-// }
-
-// // function ingredienser(ingredienser) {
-// //   ingredienser.forEach(ingredients);
-// // }
-
-// // function ingredients(ing) {
-// //   console.log("nu kan vi vise alle ingredients");
-// //   const template = document.querySelector("template").content;
-
-// //   const copy = template.cloneNode(true);
-
-// //   copy.querySelector(".ingrediens").textContent = ing.ingredient;
-// //   copy.querySelector(".amount").textContent = ing.amount + ing.type;
-
-// //   document.querySelector("main").appendChild(copy);
-// // }
